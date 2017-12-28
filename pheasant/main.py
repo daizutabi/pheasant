73b718f4508d2720a76402503938bc0926070e4a
daizutabi <daizutabi@gmail.com>
2017-12-28 23:42:58 +0900

    main

--- pheasant/main.py
+++ pheasant/main.py
@@ -3,10 +3,6 @@ import logging
 import click
 import pheasant
 
-# from pheasant.jupyter.markdown import convert as convert_markdown
-# from pheasant.jupyter.notebook import convert as convert_notebook
-# from pheasant.jupyter.client import find_kernel_names
-
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -21,37 +17,6 @@ def cli(context, version):
     else:
         if context.invoked_subcommand is None:
             click.echo(context.get_help())
-
-
-# @cli.command(name='convert', help='Convert a Markdown or Jupyter notebook.')
-# @click.argument('filename', required=True)
-# @click.option('--output_format', '-f',
-#               type=click.Choice(['html', 'markdown', 'notebook']),
-#               help='Output format.')
-# def convert_command(filename, output_format):
-#     if not os.path.exists(filename):
-#         click.echo(f'Could not find a file: {filename}')
-#         return
-#
-#     if filename.endswith('.ipynb'):
-#         markdown = convert_notebook(filename, output=output_format)
-#     else:
-#         markdown = convert_markdown(filename, output=output_format)
-#     print(markdown)
-
-
-# @cli.command(help='Show language:kernel_name infomation.')
-# def kernelname():
-#     language_kernels = find_kernel_names()
-#     language_len = max(len(language) for language in language_kernels) + 1
-#     for language in language_kernels:
-#         for k, kernel_name in enumerate(language_kernels[language]):
-#             if k == 0:
-#                 print('- ' + language + ':' +
-#                       ' ' * (language_len - len(language)),
-#                       kernel_name)
-#             else:
-#                 print(' ' * (language_len + 3), kernel_name)
 
 
 def main():
