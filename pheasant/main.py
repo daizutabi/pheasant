33b9bed8939f400dd25d27e79b60742a78fff523
daizutabi <daizutabi@gmail.com>
2017-12-28 23:08:25 +0900

    Delete command line convert

--- pheasant/main.py
+++ pheasant/main.py
@@ -4,9 +4,9 @@ import os
 import click
 import pheasant
 
-from pheasant.jupyter.markdown import convert as convert_markdown
-from pheasant.jupyter.notebook import convert as convert_notebook
-from pheasant.jupyter.client import find_kernel_names
+# from pheasant.jupyter.markdown import convert as convert_markdown
+# from pheasant.jupyter.notebook import convert as convert_notebook
+# from pheasant.jupyter.client import find_kernel_names
 
 
 logging.basicConfig(level=logging.INFO)
@@ -24,35 +24,35 @@ def cli(context, version):
             click.echo(context.get_help())
 
 
-@cli.command(name='convert', help='Convert a Markdown or Jupyter notebook.')
-@click.argument('filename', required=True)
-@click.option('--output_format', '-f',
-              type=click.Choice(['html', 'markdown', 'notebook']),
-              help='Output format.')
-def convert_command(filename, output_format):
-    if not os.path.exists(filename):
-        click.echo(f'Could not find a file: {filename}')
-        return
+# @cli.command(name='convert', help='Convert a Markdown or Jupyter notebook.')
+# @click.argument('filename', required=True)
+# @click.option('--output_format', '-f',
+#               type=click.Choice(['html', 'markdown', 'notebook']),
+#               help='Output format.')
+# def convert_command(filename, output_format):
+#     if not os.path.exists(filename):
+#         click.echo(f'Could not find a file: {filename}')
+#         return
+#
+#     if filename.endswith('.ipynb'):
+#         markdown = convert_notebook(filename, output=output_format)
+#     else:
+#         markdown = convert_markdown(filename, output=output_format)
+#     print(markdown)
 
-    if filename.endswith('.ipynb'):
-        markdown = convert_notebook(filename, output=output_format)
-    else:
-        markdown = convert_markdown(filename, output=output_format)
-    print(markdown)
 
-
-@cli.command(help='Show language:kernel_name infomation.')
-def kernelname():
-    language_kernels = find_kernel_names()
-    language_len = max(len(language) for language in language_kernels) + 1
-    for language in language_kernels:
-        for k, kernel_name in enumerate(language_kernels[language]):
-            if k == 0:
-                print('- ' + language + ':' +
-                      ' ' * (language_len - len(language)),
-                      kernel_name)
-            else:
-                print(' ' * (language_len + 3), kernel_name)
+# @cli.command(help='Show language:kernel_name infomation.')
+# def kernelname():
+#     language_kernels = find_kernel_names()
+#     language_len = max(len(language) for language in language_kernels) + 1
+#     for language in language_kernels:
+#         for k, kernel_name in enumerate(language_kernels[language]):
+#             if k == 0:
+#                 print('- ' + language + ':' +
+#                       ' ' * (language_len - len(language)),
+#                       kernel_name)
+#             else:
+#                 print(' ' * (language_len + 3), kernel_name)
 
 
 def main():
