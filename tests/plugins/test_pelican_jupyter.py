355c651f15e9e9de2981a57236ecf4cdab7cb35a
daizutabi <daizutabi@gmail.com>
2018-01-26 23:46:14 +0900

    pelican test

--- tests/plugins/test_pelican_jupyter.py
+++ tests/plugins/test_pelican_jupyter.py
@@ -36,7 +36,8 @@ def test_register():
 def test_read_markdown(reader, root):
     source_path = os.path.join(root, 'content/example.md')
     content, metadata = reader.read(source_path)
-    assert content.startswith('<h2>Stream</h2>')
+    print(content)
+    assert content.startswith('<h1>Stream</h1>')
     assert metadata['title'] == 'Pelican Example'
 
 
