a9edc0ba3c6ac7beb42d1bffc426a0d6a7030ebe
daizutabi <daizutabi@gmail.com>
2018-03-03 22:46:25 +0900

    mypy3

--- pheasant/code/converter.py
+++ pheasant/code/converter.py
@@ -1,4 +1,5 @@
-from ..utils import read_source
+# from ..utils import read_source
+from pheasant.utils import read_source
 from .inspect import convert as convert_inspect
 from .hilite import convert as convert_hilite
 
