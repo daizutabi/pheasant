8338557f539ab6e2d3878eb8d4f8adfef5568285
daizutabi <daizutabi@gmail.com>
2019-02-22 22:00:54 +0900

    test code

---
+++ tests/code/test_converter.py
@@ -0,0 +1,6 @@
+from pheasant.code.converter import initialize, convert
+
+
+def test_convert():
+    initialize()
+    assert convert('abc') == 'abc'
--- tests/code/test_html.py
+++ tests/code/test_html.py
@@ -1,12 +1,34 @@
-from pheasant.code.html import convert, escaped_code_splitter
+from pheasant.code.html import convert, escaped_code, escaped_code_splitter
 
 
 def test_convert():
+    source = 'abc'
+    assert convert(source) == source
+
     source = '```python\nprint(1)\n```\n'
     answer = '<div class=" codehilite"><pre><span></span><span class="k">'
     assert convert(source).startswith(answer)
     answer = '<span class="mi">1</span><span class="p">)</span>\n</pre></div>'
     assert convert(source).endswith(answer)
+
+    source = '```display .test\n$$x=1$$\n```\n'
+    assert convert(source) == '<div class="test">\n<p>$$x=1$$\n</p>\n</div>\n'
+
+    source = '```display .test\na & b\n```\n'
+    answer = '<div class="test">\n<p>a &amp; b</p>\n</div>\n'
+    assert convert(source) == answer
+
+    source = '~~~\n```python\nprint(1)\n```\n~~~\n'
+    answer = '<div class="codehilite pheasant-markdown pheasant-source"><pre>'
+    assert convert(source).startswith(answer)
+    answer = '1</span><span class="p">)</span>\n<span>```</span>\n</pre></div>'
+    assert convert(source).endswith(answer)
+
+
+def test_escaped_code():
+    answer = ('<div class="pheasant-markdown pheasant-code codehilite">'
+              '<pre><span></span>source\n</pre></div>')
+    assert escaped_code('abd', 'source', ['cls']) == answer
 
 
 def test_escaped_code_splitter():
