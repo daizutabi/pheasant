670c4f3b50263c4e626c224515d5da0b52fae695
daizutabi <daizutabi@gmail.com>
2019-03-17 00:18:05 +0900

    Add Code and Python rederers

--- pheasant/__init__.py
+++ pheasant/__init__.py
@@ -1,6 +1,8 @@
 __version__ = "1.2.2"
 
+from pheasant.code.renderer import Code
 from pheasant.jupyter.renderer import Jupyter
 from pheasant.number.renderer import Linker, Number
+from pheasant.python.renderer import Python
 
-__all__ = ['Jupyter', 'Linker', 'Number']
+__all__ = ["Code", "Jupyter", "Linker", "Number", "Python"]
