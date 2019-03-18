1b9a19332307bd3010d4868219a3b743e57e8729
daizutabi <daizutabi@gmail.com>
2019-03-18 17:35:41 +0900

    add convert_from_file function

--- pheasant/core/converter.py
+++ pheasant/core/converter.py
@@ -1,3 +1,4 @@
+import codecs
 from collections import OrderedDict
 from dataclasses import field
 from functools import partial
@@ -71,3 +72,12 @@ class Converter(Base):
 
     def __call__(self, *names: str) -> Callable[[str], str]:
         return partial(self.convert, names=names)
+
+    def convert_from_file(
+        self, path: str, names: Optional[Union[str, Iterable[str]]] = None
+    ) -> str:
+        with codecs.open(path, "r", "utf8") as file:
+            source = file.read()
+            source = source.replace("\r\n", "\n").replace("\r", "\n")
+
+        return self.convert(source, names)
