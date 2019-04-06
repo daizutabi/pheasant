2423ce788f919302b65a9b63562910ec22f9eb92
daizutabi <daizutabi@gmail.com>
2019-04-06 14:58:51 +0900

    docs update

--- docs/site/js/pheasant.js
+++ docs/site/js/pheasant.js
@@ -1,7 +1,12 @@
 function ready(jQuery) {
-  $('<div class="pheasant-input-button-container"><button class="pheasant-input-button"></button></div>')
+  $('<div class="pheasant-input-button-container"><button class="toggle-report-button"></button></div>')
     .appendTo(".pheasant-fenced-code .input");
-  $('.pheasant-input-button').click(function(e) {
+  $('.toggle-report-button').attr('title', 'Toggle Report');
+  $('.toggle-report-button').tooltip({hide: true, position:{
+			my:"right+5 top-28",
+			at:"right top",
+  }});
+  $('.toggle-report-button').click(function(e) {
     $('.pheasant-fenced-code').toggleClass('report-hide')
   });
 }
