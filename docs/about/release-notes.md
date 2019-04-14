06098611506412d7950c53bbf13cc78e65380995
daizutabi <daizutabi@gmail.com>
2019-04-15 08:35:33 +0900

    Delete jquery tooltip

--- docs/about/release-notes.md
+++ docs/about/release-notes.md
@@ -14,10 +14,14 @@ You can check your currently installed version using `pheasant --version`:
 
 ~~~bash terminal
 $ pheasant --version
-pheasant, version 2.2.6 from /path/to/pheasant (Python 3.7)
+pheasant, version 2.2.9 from /path/to/pheasant (Python 3.7)
 ~~~
 
 ## Change log
+
+### Version 2.2.9 (2019-04-15)
+
+* Delete jQuery tooltip for stability.
 
 ### Version 2.2.8 (2019-04-13)
 
--- pheasant/theme/js/pheasant.js
+++ pheasant/theme/js/pheasant.js
@@ -1,15 +1,4 @@
 function ready() {
-  var tooltip = {
-    hide: false,
-    track: true,
-    position: {
-      my: "center top-28",
-      at: "center top",
-    },
-    tooltipClass: "ui-tooltip-pheasant"
-  };
-
-
   function button(className) {
     return '<button class="button ' + className + '"></button>';
   };
@@ -22,7 +11,7 @@ function ready() {
   $(".pheasant-fenced-code .cell.error .code").append(container('toggle error hide'));
   $(".pheasant-fenced-code .cell.embed .code").append(container('embed'));
 
-  $('.toggle.input').attr('title', 'Toggle Status Line').tooltip(tooltip).click(function(e) {
+  $('.toggle.input').attr('title', 'Toggle Status Line').click(function(e) {
     $('.pheasant-fenced-code .input').toggleClass('hide')
   });
 
@@ -31,7 +20,7 @@ function ready() {
       var stdout = $(this).parent().find('.cell.stdout');
       if (stdout.length) {
         $(button('toggle output')).prependTo($(this).find('.pheasant-button-container'))
-          .attr('title', 'Toggle Stdout').tooltip(tooltip).click(function(e) {
+          .attr('title', 'Toggle Stdout').click(function(e) {
             stdout.toggleClass('hide');
             $(this).toggleClass('hide');
           });
@@ -39,7 +28,7 @@ function ready() {
       var stderr = $(this).parent().find('.cell.stderr').toggleClass('hide');
       if (stderr.length) {
         $(button('toggle error hide')).prependTo($(this).find('.pheasant-button-container'))
-          .attr('title', 'Toggle Stderr').tooltip(tooltip).click(function(e) {
+          .attr('title', 'Toggle Stderr').click(function(e) {
             stderr.toggleClass('hide');
             $(this).toggleClass('hide');
           });
@@ -48,7 +37,7 @@ function ready() {
 
     $(this).find('.cell.error').toggleClass('hide').each(function(index, error) {
       $(this).find('.toggle.error').each(function() {
-        $(this).attr('title', 'Toggle Traceback').tooltip(tooltip).click(function(e) {
+        $(this).attr('title', 'Toggle Traceback').click(function(e) {
           $(error).toggleClass('hide');
           $(this).toggleClass('hide');
         });
@@ -57,17 +46,17 @@ function ready() {
     });
   });
 
-  $('.pheasant-fenced-code .report .cell-count').attr('title', 'Page Execution Count').tooltip(tooltip).css("cursor", "pointer");
-  $('.pheasant-fenced-code .report .start').attr('title', 'Evaluated at').tooltip(tooltip).css("cursor", "pointer");
-  $('.pheasant-fenced-code .report .cell-time').attr('title', 'Cell Execution Time').tooltip(tooltip).css("cursor", "pointer");
-  $('.pheasant-fenced-code .report .page-time').attr('title', 'Page Execution Time').tooltip(tooltip).css("cursor", "pointer");
-  $('.pheasant-fenced-code .report .total-time').attr('title', 'Total Execution Time').tooltip(tooltip).css("cursor", "pointer");
-  $('.pheasant-fenced-code .report .total-count').attr('title', 'Total Execution Count').tooltip(tooltip).css("cursor", "pointer");
-  $('.pheasant-fenced-code .report .kernel').attr('title', 'Kernel Name').tooltip(tooltip).css("cursor", "pointer");
+  $('.pheasant-fenced-code .report .cell-count').attr('title', 'Page Execution Count').css("cursor", "pointer");
+  $('.pheasant-fenced-code .report .start').attr('title', 'Evaluated at').css("cursor", "pointer");
+  $('.pheasant-fenced-code .report .cell-time').attr('title', 'Cell Execution Time').css("cursor", "pointer");
+  $('.pheasant-fenced-code .report .page-time').attr('title', 'Page Execution Time').css("cursor", "pointer");
+  $('.pheasant-fenced-code .report .total-time').attr('title', 'Total Execution Time').css("cursor", "pointer");
+  $('.pheasant-fenced-code .report .total-count').attr('title', 'Total Execution Count').css("cursor", "pointer");
+  $('.pheasant-fenced-code .report .kernel').attr('title', 'Kernel Name').css("cursor", "pointer");
 
-  $('.source .pheasant-button-container .embed').attr('title', 'Source Code').tooltip(tooltip).css("cursor", "pointer");
-  $('.file .pheasant-button-container .embed').attr('title', 'Got from File').tooltip(tooltip).css("cursor", "pointer");
-  $('.terminal .pheasant-button-container .embed').attr('title', 'Terminal Command').tooltip(tooltip).css("cursor", "pointer");
+  $('.source .pheasant-button-container .embed').attr('title', 'Source Code').css("cursor", "pointer");
+  $('.file .pheasant-button-container .embed').attr('title', 'Got from File').css("cursor", "pointer");
+  $('.terminal .pheasant-button-container .embed').attr('title', 'Terminal Command').css("cursor", "pointer");
 }
 
 $(ready);
