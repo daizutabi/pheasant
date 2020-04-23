97d097b2fa42711b63dad454f573cda7ceeea2c6
Daizu <daizutabi@gmail.com>
2020-04-23 10:51:56 +0900

    v2.5.1

--- setup.py
+++ setup.py
@@ -97,6 +97,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Documentation",
     ],
 )
