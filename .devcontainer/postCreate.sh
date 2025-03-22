5e33f1dad94b3f9b9365c017e2fd1d8067cf06f2
Daizu <daizutabi@gmail.com>
2025-03-22 21:03:58 +0000

    Add uv.lock to .gitignore, update Python version in pyproject.toml, and modify devcontainer configuration

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,5 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
 mkdir -p ~/.config
 cp .devcontainer/starship.toml ~/.config
+
+curl -LsSf https://astral.sh/uv/install.sh | sh
+echo 'eval "$(uv generate-shell-completion bash)"' >> ~/.bashrc
